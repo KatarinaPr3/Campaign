67111635e88fd8a48c348ac66f3486344d5ba0c4
KATARINA\katarina <prljickatarina@gmail.com>
2024-10-09 17:59:07 +0200

    5: added product model for future purchase creating

--- CampaignSolution/CampaignAPI/DB/CampaignDbContext.cs
+++ CampaignSolution/CampaignAPI/DB/CampaignDbContext.cs
@@ -42,9 +42,48 @@ namespace CampaignAPI.DB
                     CampaignType = CampaignService.Enums.CampaignType.Discount
                 }
             );
+
+            modelBuilder.Entity<ProductDB>()
+                .ToTable("products")
+                .HasKey(_ => _.Id);
+
+            modelBuilder.Entity<ProductDB>().HasData(
+                new ProductDB
+                {
+                    Id = 1,
+                    Name = "Mobile phone Samsung S24",
+                    Price = 1500
+
+                },
+                new ProductDB
+                {
+                    Id = 2,
+                    Name = "Mobile phone Samsung S23",
+                    Price = 1000
+
+                },
+                new ProductDB
+                {
+                    Id = 3,
+                    Name = "Laptop Acer Nitro",
+                    Price = 2000
+
+                },
+                new ProductDB
+                {
+                    Id = 4,
+                    Name = "Mobile phone Samsung S22",
+                    Price = 500
+
+                },
+                new ProductDB
+                {
+                    Id = 5,
+                    Name = "Sim Card",
+                    Price = 5
+
+                }
+            );
         }
-
-
-
     }
 }
